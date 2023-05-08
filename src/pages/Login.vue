358036eb4e479d12b9c0c1f00e259878c8b4aff4
Thiago Rodrigues da Silva <thiago.rodrigues@fieb.org.br>
2023-05-08 20:13:01 -0300

    Add: btn login and register

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,22 +1,52 @@
 <template>
   <q-page padding>
     <q-form class="row justify-center">
-      <p class="col-12 text-h5 text-center">
-        Login
+      <p class="col-12 text-h5 text-center" @submit.prevent="">
+        Área de Login
       </p>
       <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
+        <!-- Input -->
         <q-input
           label="Email"
           v-model="form.email"
           standout="bg-blue text-white"
+          type="email"
         />
 
         <q-input
-          label="Password"
+          label="Senha"
           v-model="form.password"
           standout="bg-blue text-white"
           type="password"
         />
+        <!-- Input -->
+
+        <!-- Btn Login-->
+        <div class="full-width q-pt-md">
+          <q-btn
+          label="Login"
+          color="primary"
+          class="full-width"
+          icon-right="login"
+          outline
+          rounded
+          type="submit"
+          />
+        </div>
+
+        <!-- Btn register
+        <div class="full-width">
+          <q-btn
+          label="Cadastrar"
+          color="primary"
+          class="full-width"
+          icon-right="send"
+          flat
+          rounded
+          to="/register"
+          />
+        </div> -->
+
       </div>
     </q-form>
   </q-page>
